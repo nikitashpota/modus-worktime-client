3706b1cd97a742b0c70e4752f1f6be7df6831b8e
User <nikitashpota@ya.ru>
2024-03-24 15:31:28 +0300

    0.0.4

--- src/views/DirectorDashboard.jsx
+++ src/views/DirectorDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "../services/axios";
-import { Tab, Row, Col, Nav } from "react-bootstrap";
+import { Tabs, Tab, Row, Col, Nav } from "react-bootstrap";
 import WorkLogSummaryTable from "../components/WorkLogSummaryTable";
 import EmployeeWorkloadTable from "../components/EmployeeWorkloadTable";
 import ProjectOverview from "../components/ProjectOverview";
@@ -39,56 +39,82 @@ const DirectorDashboard = () => {
   }, []);
 
   return (
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
-        <Col sm={2}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Ведомость дней-часов</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">Ведомость объектов</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="third">Пользователь-задачи</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="fourth">Отдел-часы</Nav.Link>
-            </Nav.Item>
-            {/* Добавьте другие Nav.Item с соответствующими Nav.Link для каждой таблицы или графика */}
-          </Nav>
-        </Col>
-        <Col sm={9}>
-          <Tab.Content>
-            <Tab.Pane eventKey="first">
-              <WorkLogSummaryTable />
-            </Tab.Pane>
-            <Tab.Pane eventKey="second">
-              <EmployeeWorkloadTable
-                users={users}
-                buildings={buildings}
-                workTimeLogs={workTimeLogs}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="third">
-              <ProjectOverview
-                users={users}
-                buildings={buildings}
-                workTimeLogs={workTimeLogs}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="fourth">
-              <DepartmentHoursChart
-                users={users}
-                buildings={buildings}
-                workTimeLogs={workTimeLogs}
-              />
-            </Tab.Pane>
-            {/* Добавьте другие Tab.Pane для каждой таблицы или графика */}
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+    <Tabs
+      defaultActiveKey="workLogSummaryTable"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="workLogSummaryTable" title="День-час">
+        <WorkLogSummaryTable />
+      </Tab>
+      <Tab eventKey="employeeWorkloadTable" title="Объект-проценты">
+        <EmployeeWorkloadTable
+          users={users}
+          buildings={buildings}
+          workTimeLogs={workTimeLogs}
+        />
+      </Tab>
+      <Tab eventKey="erojectOverview" title="Польз.-задачи">
+        <ProjectOverview
+          users={users}
+          buildings={buildings}
+          workTimeLogs={workTimeLogs}
+        />
+      </Tab>
+      <Tab eventKey="departmentHoursChart" title="Отдел-часы">
+        <DepartmentHoursChart
+          users={users}
+          buildings={buildings}
+          workTimeLogs={workTimeLogs}
+        />
+      </Tab>
+    </Tabs>
+    // <Tab.Container id="top-tabs-example" defaultActiveKey="first">
+    //   <Row>
+    //     <Col sm={2}>
+    //       <Nav variant="pills" className="flex-column">
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="first">Ведомость дней-часов</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="second">Ведомость объектов</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="third">Пользователь-задачи</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="fourth">Отдел-часы</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </Col>
+    //     <Col sm={9}>
+    //       <Tab.Content>
+    //         <Tab.Pane eventKey="first">
+    //           <WorkLogSummaryTable />
+    //         </Tab.Pane>
+    //         <Tab.Pane eventKey="second">
+    //           <EmployeeWorkloadTable
+    //             users={users}
+    //             buildings={buildings}
+    //             workTimeLogs={workTimeLogs}
+    //           />
+    //         </Tab.Pane>
+    //         <Tab.Pane eventKey="third">
+
+    //           />
+    //         </Tab.Pane>
+    //         <Tab.Pane eventKey="fourth">
+    //           <DepartmentHoursChart
+    //             users={users}
+    //             buildings={buildings}
+    //             workTimeLogs={workTimeLogs}
+    //           />
+    //         </Tab.Pane>
+    //         {/* Добавьте другие Tab.Pane для каждой таблицы или графика */}
+    //       </Tab.Content>
+    //     </Col>
+    //   </Row>
+    // </Tab.Container>
   );
 };
 
