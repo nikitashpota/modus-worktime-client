f7cbc3e5cac77d986f628f1f9d886f99df1da11e
User <nikitashpota@ya.ru>
2024-04-12 09:11:46 +0300

    0.1.01

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div style={{ height: '20px', backgroundColor: '#f8f9fa' }}>
       <Container className="text-center" style={{ lineHeight: '20px', fontSize: '12px' }}>
-       | ОАО "Модус" 2024  | Версия 0.0.1 |
+       | ОАО "Модус" 2024  | Версия 0.1.0 |
       </Container>
     </div>
   );
